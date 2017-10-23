1e8c04c0a9ca1e6cf863cc90a5f18f0d2a2b6169
bdegley4789 <egleyb@oregonstate.edu>
2017-10-23 14:34:41 -0700

    Add files via upload

---
+++ a02_concurrency/main.c
@@ -0,0 +1,75 @@
+/* CS 444
+* Dining Philosopher Problem
+* OCT 23, 2017
+*
+*
+*/
+
+#include <stdio.h>
+#include <pthread.h>
+
+void *plato(void* vargp)
+{
+	// Think
+
+	// get_fork
+
+	// eat
+
+	// put_fork
+}
+
+void *aristotle(void* vargp)
+{
+	// Think
+
+	// get_fork
+
+	// eat
+
+	// put_fork
+
+}
+
+void *socrates(void* vargp)
+{
+	// Think
+
+	// get_fork
+
+	// eat
+
+	// put_fork
+
+}
+
+void *confucius(void* vargp)
+{
+	// Think
+
+	// get_fork
+
+	// eat
+
+	// put_fork
+
+}
+
+void *epicurus(void* vargp)
+{
+	// Think
+
+	// get_fork
+
+	// eat
+
+	// put_fork
+}
+
+int main() {
+
+	pthread_t pid;
+
+	return 0;
+}
+
