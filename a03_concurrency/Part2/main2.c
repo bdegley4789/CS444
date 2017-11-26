9a88af3ea3d9ac54a30ce075a516d0e69a386359
bdegley4789 <egleyb@oregonstate.edu>
2017-11-26 01:02:33 -0800

    Created inserters and deleters

--- a03_concurrency/Part2/main2.c
+++ a03_concurrency/Part2/main2.c
@@ -1,8 +1,8 @@
 //Bryce Egley and Bruce Garcia
 //CS 444
-//Concurrency exercise 3:
-//We re-used some of the code from concurrency exercise #1
-// Source: http://www.zentut.com/c-tutorial/c-linked-list/ 
+//Concurrency exercise 3: Part 2
+//We re-used some of the code from concurrency exercise #1 and #2
+// Source: http://www.zentut.com/c-tutorial/c-linked-list/
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
@@ -14,32 +14,41 @@
 #include <assert.h>
 #define maxSize 5 //Table will hold 5 plates and forks
 
-typedef struct status {
-    int fork;
-    char status[15];
-} Status;
+typedef enum{ON,OFF} status_t;
 
-// Linked list structure 
+char names [maxSize][15] = {"searcher", "inserter","deleter"};
+//Linked List of int's
 typedef struct node {
-	int data;
-	struct node* next;
+    int data;
+    struct node* next;
 } node;
+int count;
+node *head;
+status_t state_deleter = OFF;
+status_t state_inserter = OFF;
 
-
-Status arrPhilosophers[maxSize];
-
-char names [maxSize][15] = {"plato", "aristotle","socrates","confucius","epicurus"};
-
-pthread_cond_t thread_plato, thread_aristotle, thread_socrates, thread_confucius, thread_epicurus;
-pthread_cond_t thread_selector, thread_inserter, thread_deleter;
+pthread_cond_t thread_searcher, thread_inserter, thread_deleter;
 pthread_mutex_t thread_mutex;
 
+void think() {
+    //Think for 1-20 seconds
+    unsigned int think_sleep = (genrand_int32() % 20) + 1;
+    while(think_sleep)
+        think_sleep = sleep(think_sleep);
+}
+void execute() {
+    //Eat for 2-9 seconds
+    unsigned int eat_sleep = (genrand_int32() % 8) + 2;
+    while(eat_sleep)
+        eat_sleep = sleep(eat_sleep);
+}
+//Insert new element to end of linked list
 /*
-    create a new node
-    initialize the data and next field
+ create a new node
+ initialize the data and next field
  
-    return the newly created node
-*/
+ return the newly created node
+ */
 node* create(int data,node* next)
 {
     node* new_node = (node*)malloc(sizeof(node));
@@ -50,22 +59,19 @@ node* create(int data,node* next)
     }
     new_node->data = data;
     new_node->next = next;
- 
+    
     return new_node;
 }
-
 /*
-    add a new node at the beginning of the list
-*/
-node* prepend(node* head,int data)
+ add a new node at the beginning of the list
+ */
+node* prepend(node* theHead,int data)
 {
-    node* new_node = create(data,head);
-    head = new_node;
-    return head;
+    node* new_node = create(data,theHead);
+    theHead = new_node;
+    return theHead;
 }
-/*
-    add a new node at the end of the list
-*/
+//Add new data to end of linked list
 node* append(node* head, int data)
 {
     if(head == NULL)
@@ -74,240 +80,195 @@ node* append(node* head, int data)
     node *cursor = head;
     while(cursor->next != NULL)
         cursor = cursor->next;
- 
+    
     /* create a new node */
     node* new_node =  create(data,NULL);
     cursor->next = new_node;
- 
+    
     return head;
 }
-
-/*
-    Search for a specific node with input data
- 
-    return the first matched node that stores the input data,
-    otherwise return NULL
-*/
-node* search(node* head,int data)
+int find(node* theHead, int position)
 {
- 
-    node *cursor = head;
-    while(cursor!=NULL)
-    {
-        if(cursor->data == data)
-            return cursor;
+    if(theHead==NULL)
+        return 0;
+    /* Go to position */
+    node *cursor = theHead;
+    int i;
+    for(i = 0; i < position; i++) {
         cursor = cursor->next;
     }
-    return NULL;
+    /* Get Data */
+    return cursor->data;
 }
 
-/*
-    Display all nodes in the linked list
-*/
-void displayAll(node *head)
-{
-    node *cursor = head;
-
-    while(cursor != NULL)
-    {
-        printf("Node data: %d\n", cursor->data);
-        cursor = cursor->next;
-    }
-}
-/*
-    Create a link list of five elements
-*/
+// Create a link list of five elements
 node* create_struct(node* theHead) {
-	
-	int iterations = 5;
-	int i;
+    
+    int iterations = 5;
+    int i;
     init_genrand(time(NULL));
-
+    
     // Generate a number between 1-20
-    int number = (genrand_int32() % 20) + 1;    
+    int number = (genrand_int32() % 20) + 1;
     theHead = prepend(theHead, number);
-
-
-	for(i = 0; i < iterations; i++) { 
-		// Generate a number between 2-21
-        number = (genrand_int32() % 20) + 1;    
-		theHead =  append(theHead, number);
-	}
-
-	return theHead;
-};
-
-
+    
+    for(i = 0; i < iterations; i++) {
+        // Generate a number between 1-20
+        number = (genrand_int32() % 20) + 1;
+        theHead =  append(theHead, number);
+        printf("Current value %d\n", theHead->data);
+    }
+    return theHead;
+}
+void delete(int position)
+{
+    if(head == NULL)
+        printf("Empty linked list");
+    /* Go to position */
+    node *cursor = head;
+    int i;
+    //Stop one before
+    for(i = 0; i < position-1; i++) {
+        cursor = cursor->next;
+    }
+    /* delete node */
+    cursor->next = cursor->next->next;
+}
+//Add new data to end of linked list
+void append2(int data)
+{
+    if(head == NULL)
+        printf("Empty linked list");
+    /* go to the last node */
+    node *cursor = head;
+    while(cursor->next != NULL)
+        cursor = cursor->next;
+    
+    /* create a new node */
+    node* new_node =  create(data,NULL);
+    cursor->next = new_node;
+}
+int search() {
+    unsigned int searchPos = genrand_int32() % count;
+    return find(head,searchPos);
+}
+int insert_item() {
+    //Random data 0-99999
+    unsigned int data = genrand_int32() % 100000;
+    //Add new data to end of node
+    append2(data);
+    count++;
+    return data;
+}
+int delete_item() {
+    //Position to delete
+    unsigned int deletePos = genrand_int32() % count;
+    //Find data about to be deleted
+    int data = find(head,deletePos);
+    //Delete node at specific position
+    delete(deletePos);
+    //Decrement count
+    count--;
+    return data;
+}
 // Searchers
 // They merely examine the list; hence thye can execute concurrently w/ each other
 
-void *searchers(void* ptr)
-{
+/*void *searchers(void* ptr)
+ {
+ 
+ node * head;
+ head = (node *) ptr;
+ while(1) {
+ // searching the linked list for data
+ printf("Searching...\n");
+ int data = (genrand_int32() % 20) + 1;
+ node *temp = search(head, data);
+ printf("Searching...\n");
+ if (temp != NULL)
+ {
+ printf("temp value = %d\n", temp->data);
+ }
+ }
+ }*/
 
-    node * head;
-    head = (node *) ptr;
+void *searcher(void* ptr)
+{
     while(1) {
-        // searching the linked list for data
-        printf("Searching...\n");
-        int data = (genrand_int32() % 20) + 1;    
-        node *temp = search(head, data);
-        printf("Searching...\n");
-        if (temp != NULL)
-        {
-            printf("temp value = %d\n", temp->data);
+        sleep(1);
+        if(state_deleter == ON) {
+            printf("Waiting for deleter to finish");
         }
+        think();
+        printf("Item %d found\n",search());
+        execute();
     }
 }
 
 // Inserters
-// They add new items to the end of the list; they must be mutually exclusive to preclude 
+// They add new items to the end of the list; they must be mutually exclusive to preclude
 // two inserters from inserting new items at about the same time.
+void *inserter(void* ptr)
+{
+    while(1) {
+        sleep(1);
+        if(state_deleter == ON) {
+            printf("Waiting for deleter to finish");
+        }
+        if(state_inserter == ON) {
+            printf("Waiting for inserter to finish");
+        }
+        think();
+        state_inserter = ON;
+        pthread_mutex_lock(&thread_mutex);
+        printf("Item %d inserted\n",insert_item());
+        pthread_cond_signal(&thread_inserter);
+        pthread_mutex_unlock(&thread_mutex);
+        state_inserter = OFF;
+        execute();
+    }
+}
 
 // Deleters
 // They remove items from anywhre in the list. At most one deleter process can access
 // the list at a time, and deletion must also be mutually exclusive with searches and insertions.
-
-void think() {
-    //Think for 1-20 seconds
-    unsigned int think_sleep = (genrand_int32() % 20) + 1;
-    while(think_sleep)
-        think_sleep = sleep(think_sleep);
-}
-void eat() {
-    //Eat for 2-9 seconds
-    unsigned int eat_sleep = (genrand_int32() % 8) + 2;
-    while(eat_sleep)
-        eat_sleep = sleep(eat_sleep);
-}
-//Print current results
-void print_results() {
-    printf("Fork Status\n");
-    int i;
-    for (i = 0; i < maxSize; i++) {
-        if (arrPhilosophers[i].fork == 1) {
-            printf("Fork %d is currently held by %s\n",i+1,names[i]);
-        } else {
-            printf("Fork %d is currently not being held\n",i+1);
+void *deleter(void* ptr)
+{
+    while(1) {
+        sleep(1);
+        if(state_deleter == ON) {
+            printf("Waiting for deleter to finish");
         }
-    }
-    printf("Philosopher Status\n");
-    int j;
-    for (j = 0; j < maxSize; j++) {
-        printf("%s is currently %s\n",names[j],arrPhilosophers[j].status);
-    }
-    printf("***************************************\n");
-}
-void get_forks(int n, pthread_cond_t phil) {
-    //Change philospher status
-    Status getFork;
-    //0 for false since you don't have the fork yet
-    getFork = (Status){.status = "Getting Fork",.fork = 0};
-    pthread_mutex_lock(&thread_mutex);
-    arrPhilosophers[n] = getFork;
-    //Print new status
-    print_results();
-    pthread_mutex_unlock(&thread_mutex);
-    //Change Status
-    getFork = (Status){.status = "Eating",.fork = 1};
-    arrPhilosophers[n] = getFork;
-    //Lock Mutex for thread use
-    pthread_mutex_lock(&thread_mutex);
-    pthread_cond_signal(&phil);
-    ////Unlock mutex so other threads can use data structure
-    pthread_mutex_unlock(&thread_mutex);
-    //Output new results
-    print_results();
-}
-void put_forks(int n, pthread_cond_t phil) {
-    //Change philospher status
-    Status putFork;
-    //1 for true since you still have the fork
-    putFork = (Status){.status = "Putting Fork",.fork = 1};
-    pthread_mutex_lock(&thread_mutex);
-    arrPhilosophers[n] = putFork;
-    //Print new status
-    print_results();
-    pthread_mutex_unlock(&thread_mutex);
-    //Change Status
-    putFork = (Status){.status = "Thinking",.fork = 0};
-    arrPhilosophers[n] = putFork;
-    //Lock Mutex for thread use
-    pthread_mutex_lock(&thread_mutex);
-    pthread_cond_signal(&phil);
-    ////Unlock mutex so other threads can use data structure
-    pthread_mutex_unlock(&thread_mutex);
-    //Output new results
-    print_results();
-}
-
-void *plato(void* ptr)
-{
-    while(1) {
+        if(state_inserter == ON) {
+            printf("Waiting for inserter to finish");
+        }
         think();
-        get_forks(0,thread_plato);
-        eat();
-        put_forks(0,thread_plato);
-    }
-}
-
-void *aristotle(void* ptr)
-{
-    while(1) {
-        think();
-        get_forks(1,thread_aristotle);
-        eat();
-        put_forks(1,thread_aristotle);
-    }
-}
-
-void *socrates(void* ptr)
-{
-    while(1) {
-        think();
-        get_forks(2,thread_socrates);
-        eat();
-        put_forks(2,thread_socrates);
-    }
-}
-
-void *confucius(void* ptr)
-{
-    while(1) {
-        think();
-        get_forks(3,thread_confucius);
-        eat();
-        put_forks(3,thread_confucius);
-    }
-}
-
-void *epicurus(void* ptr)
-{
-    while(1) {
-        think();
-        get_forks(4,thread_epicurus);
-        eat();
-        put_forks(4,thread_epicurus);
+        state_deleter = ON;
+        pthread_mutex_lock(&thread_mutex);
+        while (count <= 0) {
+            printf("LinkedList is empty. Waiting for inserter");
+            return 0;
+        }
+        pthread_mutex_unlock(&thread_mutex);
+        pthread_mutex_lock(&thread_mutex);
+        printf("Item %d deleted\n",delete_item());
+        //Unlock mutex for shared resource
+        pthread_cond_signal(&thread_deleter);
+        pthread_mutex_unlock(&thread_mutex);
+        state_deleter = OFF;
+        execute();
     }
 }
 
 int main() {
     //Have seed generate for random sequnce of numbers with genrand
     init_genrand(time(NULL));
-    int data = 10;
-	node *head = NULL;
-
-    // Creates linked list with 5 nodes. 
-	head = create_struct(head);	
-    displayAll(head);
-
+    head = NULL;
+    head = create_struct(head);
     // Thread ID.
-    pthread_t tidPlato;
-    pthread_t tidAristotle;
-    pthread_t tidSocrates;
-    pthread_t tidConfucius;
-    pthread_t tidEpicurus;
-
-    pthread_t tidSelecter;
+    pthread_t tidSearcher;
+    pthread_t tidInserter;
+    pthread_t tidDeleter;
     
     //Create mutex so threads can both use shared resource
     if(pthread_mutex_init(&thread_mutex, NULL)) {
@@ -316,47 +277,28 @@ int main() {
     }
     
     //Initialize conditional vars so thread can wait until condition occurs
-    pthread_cond_init(&thread_plato, NULL);
-    pthread_cond_init(&thread_aristotle, NULL);
-    pthread_cond_init(&thread_socrates, NULL);
-    pthread_cond_init(&thread_confucius, NULL);
-    pthread_cond_init(&thread_epicurus, NULL);
-
-    pthread_cond_init(&thread_selector, NULL);
+    pthread_cond_init(&thread_searcher, NULL);
+    pthread_cond_init(&thread_inserter, NULL);
+    pthread_cond_init(&thread_deleter, NULL);
     
     // Create consumer and producer thread.
-    pthread_create(&tidPlato, NULL, plato, NULL);
-    printf("PLato thread created.\n");
-    pthread_create(&tidAristotle, NULL, aristotle, NULL);
-    printf("Aristotle thread created.\n");
-    pthread_create(&tidSocrates, NULL, socrates, NULL);
-    printf("Socrates thread created.\n");
-    pthread_create(&tidConfucius, NULL, confucius, NULL);
-    printf("Confucius thread created.\n");
-    pthread_create(&tidEpicurus, NULL, epicurus, NULL);
-    printf("Epicurus thread created.\n");
-    printf("***************************************\n");
-
-    pthread_create(&tidSelecter, NULL, searchers, (void *) &head);
+    pthread_create(&tidSearcher, NULL, searcher, NULL);
+    printf("Searcher thread created.\n");
+    pthread_create(&tidInserter, NULL, inserter, NULL);
+    printf("Inserter thread created.\n");
+    pthread_create(&tidDeleter, NULL, deleter, NULL);
+    printf("Deleter thread created.\n");
     
     // When done join threads.
-    pthread_join(tidPlato, NULL);
-    pthread_join(tidAristotle, NULL);
-    pthread_join(tidSocrates, NULL);
-    pthread_join(tidConfucius, NULL);
-    pthread_join(tidEpicurus, NULL);
-
-    pthread_join(tidSelecter, NULL);
+    pthread_join(tidSearcher, NULL);
+    pthread_join(tidInserter, NULL);
+    pthread_join(tidDeleter, NULL);
     
     //Destroy pthreads so they don't continue after ctrl c
     pthread_mutex_destroy(&thread_mutex);
-    pthread_cond_destroy(&thread_plato);
-    pthread_cond_destroy(&thread_aristotle);
-    pthread_cond_destroy(&thread_socrates);
-    pthread_cond_destroy(&thread_confucius);
-    pthread_cond_destroy(&thread_epicurus);
-
-    pthread_cond_destroy(&thread_selector);
+    pthread_cond_destroy(&thread_searcher);
+    pthread_cond_destroy(&thread_inserter);
+    pthread_cond_destroy(&thread_deleter);
     printf("Threads are completed.\n");
     
     exit(0);
